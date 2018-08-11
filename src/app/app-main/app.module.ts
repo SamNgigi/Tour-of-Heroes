c77e3defe20da0e9bb16154d6311905fd34fd98c
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-08-11 08:46:04 +0300

    Hero detail working

--- src/app/app-main/app.module.ts
+++ src/app/app-main/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from '../heroes/heroes.component';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   // Here in the imports array we add a list of external modules that the app needs.
   imports: [
