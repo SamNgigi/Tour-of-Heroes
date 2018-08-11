87749d40053b779c357a6f22c04bf03e93547a1e
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-08-11 09:52:36 +0300

    Input property binding working. Also added material design form

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+// Note that we add the Input decorator
+import { Component, OnInit, Input } from '@angular/core';
+// We import our Hero blueprint.
+import { Hero } from '../Hero';
+
 
 @Component({
   selector: 'toh-hero-detail',
@@ -6,6 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
+  /* 
+    The @Input() decorator indicates property binding.
+
+    What is happening here is that the hero property here receives the
+    hero object from the parent component HeroComponent using the Input()
+    decorator.
+  */
+  @Input() is_selected_hero: Hero;
 
   constructor() { }
 
