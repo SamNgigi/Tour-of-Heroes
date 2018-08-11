43a2288d496d39722e5279addd76f6530ebbe7fc
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-08-11 10:42:44 +0300

    Hero service working and changed the focus color of the mdb-form

--- src/app/hero-service/hero.service.ts
+++ src/app/hero-service/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+// We import our Hero blueprint
+import { Hero } from '../Hero';
+// We import the HEROES array of Hero object instances.
+import { HEROES } from '../heroes-array';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
   constructor() { }
+
+  // We add a getHeroes function
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 // We import our Hero Class 
 import { Hero } from "../Hero";
-// We import our HEROES object array.
-import { HEROES } from "../heroes-array";
+/* 
+  We edit this out because we have a hero service dealing with this.
+  
+  // We import our HEROES object array.
+  import { HEROES } from "../heroes-array";
+ */
+// We import our hero-service
+import { HeroService } from '../hero-service/hero.service';
 
 /* 
+
   The @Component is a decorator function that specifies the Angular
   metadata for a component.
 */
@@ -24,24 +31,41 @@ export class HeroesComponent implements OnInit {
 
   // We add a hero property.
 
-  // hero = 'Ciise Masiix';
+  // hero = 'Ciise Masiix'; 
 
-  // Defining it using our class
+  // Defining the var to hold the array it using our class
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selected_hero: Hero;
 
   /* 
-    This function takes in the hero form the *ngFor repeater directive
-    that is selected. Passes it in-place of our hero parameter which is
-    of type Hero. Finally we assign its value to selected_hero.
+    It seems like i should call my constructor below property declaration.
   */
+  constructor(private heroService: HeroService) {
+    // Calling functions in the constructor is not  best practice.
+
+    /* 
+      We reserve the constructor for simple initialization such as
+      writing constructor parameters and properties.
+
+      The constructor should not do anything. It certainly should not
+      make HTTP requests to a remote server as a real data service would.
+    */
+  }
+
+  /* 
+   This function takes in the hero form the *ngFor repeater directive
+   that is selected. Passes it in-place of our hero parameter which is
+   of type Hero. Finally we assign its value to selected_hero.
+ */
   onSelect(hero: Hero): void {
     this.selected_hero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   /* 
     Below is an Angular life-cycle hook called immediately after a
     component is created.
@@ -49,6 +73,7 @@ export class HeroesComponent implements OnInit {
     It is a good place to put initialization logic.
   */
   ngOnInit() {
+    this.getHeroes()
   }
 
 }
